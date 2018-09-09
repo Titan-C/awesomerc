db45444e7a61d3d5e08ae28e8d7746797a16be2a
Óscar Nájera <hello@oscarnajera.com>
2018-09-09 14:36:50 +0200

    Prefer fair splitting

--- rc.lua
+++ rc.lua
@@ -72,11 +72,11 @@ local modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
+    awful.layout.suit.fair,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
-    awful.layout.suit.fair,
     awful.layout.suit.fair.horizontal,
     -- awful.layout.suit.spiral,
     -- awful.layout.suit.spiral.dwindle,
