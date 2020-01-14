a35d157e071894a74eb34da8b99c68f9556ee9bc
Óscar Nájera <oscar.najera@tribe29.com>
2020-01-14 09:42:11 +0100

    New docking station screen connections

--- rc.lua
+++ rc.lua
@@ -49,7 +49,9 @@ if "titanroam" == hostname then
 end
 
 if "klappbier" == hostname then
-    awful.spawn.with_shell("xrandr --output DP-2-1 --off --output DP-2-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-2-3 --off --output eDP-1 --primary --mode 1920x1080 --pos 1584x1440 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --off")
+    awful.spawn.with_shell("xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1584x1440 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 2560x1440 --pos 0x0 --rotate normal")
+
+
 end
 
 -- {{{ Variable definitions
