043745a893ed6991ec5fd83ffe17553c3ac4853b
Óscar Nájera <oscar.najera@tribe29.com>
2020-03-19 09:26:35 +0100

    HomeOffice screen

--- rc.lua
+++ rc.lua
@@ -49,9 +49,7 @@ if "titanroam" == hostname then
 end
 
 if "klappbier" == hostname then
-    awful.spawn.with_shell("xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1584x1440 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 2560x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-1 --off --output DP-1-3 --off --output DP-2 --off --output DP-1-1 --mode 2560x1440 --pos 0x0 --rotate normal")
-
-
+    awful.spawn.with_shell("xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2560x1080 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 2560x0 --rotate normal")
 end
 
 -- {{{ Variable definitions
