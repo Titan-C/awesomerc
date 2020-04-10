9a948c2dd4161c0db29b66a81440946f35bee34f
Óscar Nájera <oscar.najera@tribe29.com>
2020-04-11 00:13:06 +0200

    Desk organization

--- rc.lua
+++ rc.lua
@@ -49,7 +49,7 @@ if "titanroam" == hostname then
 end
 
 if "klappbier" == hostname then
-    awful.spawn.with_shell("xrandr --output eDP-1 --primary --mode 1920x1080 --pos 2560x1080 --rotate normal --output DP-1-2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 2560x0 --rotate normal")
+    awful.spawn.with_shell("xrandr --output eDP-1 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-1-3 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1-1 --primary --mode 2560x1440 --pos 1920x0 --rotate normal")
 end
 
 -- {{{ Variable definitions
